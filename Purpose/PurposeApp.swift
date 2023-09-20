40eeb8d8268661959d17995ac5edfc6a86675707
Joseph DeWeese <jdeweese@weldtascics.systems>
2023-09-20 17:17:52 -0500

    Initial Commit

---
+++ Purpose/PurposeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PurposeApp.swift
+//  Purpose
+//
+//  Created by J. DeWeese on 9/20/23.
+//
+
+import SwiftUI
+
+@main
+struct PurposeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
